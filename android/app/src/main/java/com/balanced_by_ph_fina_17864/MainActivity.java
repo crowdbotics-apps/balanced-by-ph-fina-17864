86e198fde78e848e5a1b93ab978f9bcbf2af5d06
Crowdbotics <team@crowdbotics.com>
2020-06-05 21:57:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/balanced_by_ph_fina_17864/MainActivity.java
@@ -0,0 +1,15 @@
+package com.balanced_by_ph_fina_17864;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "balanced_by_ph_fina_17864";
+    }
+}
